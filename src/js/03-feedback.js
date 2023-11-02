f04729f50484b9a41e6ad522b4a479a90f6287d4
Igor-Principal <igorprincipal@gmail.com>
2023-11-02 23:53:17 +0200

    create part of task 2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,13 @@
+const form = document.querySelector('.feedback-form');
+
+form.addEventListener('input', inInput);
+
+function inInput(evt) {
+  const inputName = evt.target.name;
+  const inputValue = evt.target.value;
+  localStorage.setItem(inputName, inputValue);
+}
+
+
+
 
