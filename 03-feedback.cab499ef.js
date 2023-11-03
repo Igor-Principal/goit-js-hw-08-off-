656b87195d1670b62d43c1c948756f9f10e29a31
Igor-Principal <igorprincipal@gmail.com>
2023-11-03 17:54:56 +0000

    Deploying to gh-pages from @ Igor-Principal/goit-js-hw-08.@046ae96b53afca4320f385377eff159ef9842455 🚀

--- 03-feedback.cab499ef.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("input",(function(e){var t=e.target.name,a=e.target.value;localStorage.setItem(t,a)}));
-//# sourceMappingURL=03-feedback.cab499ef.js.map
